e60347ba89ce7875455fea0e6287bf3e2c7e41b7
MacKhai23 <87077992+MacKhai23@users.noreply.github.com>
2021-11-02 17:43:32 +0200

    Add files via upload

---
+++ jscode.js
@@ -0,0 +1,17 @@
+function validateTextbox() {
+ 
+    var box = document.getElementById("name");
+     if (box.value == "") {
+    alert("The field marked in red cannot be blank");
+    return false;
+     }
+     
+     }function validateTextbox() {
+ 
+        var box = document.getElementById("Phone");
+         if (box.value == "") {
+        alert("The field marked in red cannot be blank");
+        return false;
+         }
+         
+         }
